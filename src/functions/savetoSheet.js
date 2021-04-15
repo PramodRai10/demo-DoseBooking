dfe9d666d15c997fc1a8d748035cd81bb0b8c7af
PramodRai10 <p10pramodr@gmail.com>
2021-04-15 20:50:45 +0530

    SheetAdded

--- src/functions/savetoSheet.js
+++ src/functions/savetoSheet.js
@@ -1,59 +1,31 @@
-// const { GoogleSpreadsheet } = require('google-spreadsheet')
-    
-//     exports.handler = async (event, context, callback) => {
-//       try {
-//         const doc = new GoogleSpreadsheet('some_google_document_id')
-//         await doc.useServiceAccountAuth(require('./your-service-account.json'))
-//         await doc.loadInfo()
-//         const sheet = doc.sheetsByIndex[0]
-    
-//         //   const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
-// //   const appendSpreadsheet = async (row) => {
-// //     try {
-// //       await doc.useServiceAccountAuth({
-// //         client_email: CLIENT_EMAIL,
-// //         private_key: PRIVATE_KEY.replace(/\\n/g, '\n'),
-// //       });
-// //       // loads document properties and worksheets
-// //       await doc.loadInfo();
+  const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
+  const SHEET_ID = process.env.REACT_APP_SHEET_ID;
+  const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
+  const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
 
-// //       const sheet = doc.sheetsById[SHEET_ID];
-// //       await sheet.addRow(row);
-// //       setData({ ...data, Name: '', Email: '', DOB: '1940', AadharId: null, Phone: '', Vaccination_hospital: 'Starcity Hospital', Vaccination_date: '' });
-// //       props.hideFn(true);
-
-// //     } catch (e) {
-// //       console.error('Error: ', e);
-// //     }
-// //   };
-
-//         const data =JSON.parse(event.body)
-//         const addedRow = await sheet.addRow(data)
-    
-     
-//         return {
-//           statusCode: 200,
-//           body: JSON.stringify({
-//             message: `row added`,
-//           }),
-//         }
-//       } catch (e) {
-//         return {
-//           statusCode: 500,
-//           body: e.toString(),
-//         }
-//       }
-//     }
-
-// For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
-export function handler(event, context, callback) {
-    console.log("queryStringParameters", event.queryStringParameters)
-    callback(null, {
-      // return null to show no errors
-      statusCode: 200, // http status code
-      body: JSON.stringify({
-        msg: "Hello, World! " + Math.round(Math.random() * 10),
-      }),
-    })
-  }
+    exports.handler = async (event, context, callback) => {
+      try {
+        const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
+        await doc.useServiceAccountAuth({
+                client_email: CLIENT_EMAIL,
+               private_key: PRIVATE_KEY.replace(/\\n/g, '\n'),
+            });
+        await doc.loadInfo();
+        const sheet = doc.sheetsByIndex[SHEET_ID];
+        const data =JSON.parse(event.body)
+        await sheet.addRow(row);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: `row added`,
+          }),
+        }
+      } catch (e) {
+        return {
+          statusCode: 500,
+          body: e.toString(),
+        }
+      }
+    }
