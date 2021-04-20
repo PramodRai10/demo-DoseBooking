7beff11ed4ff5a168beff44a525e49834c9eb322
PramodRai10 <p10pramodr@gmail.com>
2021-04-20 18:09:39 +0530

    Improved Loader responsive

--- src/SpinnerPage.js
+++ src/SpinnerPage.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import LoadingOverlay from "react-loading-overlay";
 import "./SpinnerPage.css";
 
 function SpinnerPage() {
     return (
-        <div className="darkBg">
-            <LoadingOverlay
-                active={true}
-                // spinner={<BounceLoader />}
-                spinner={true}
-                text="Please Hold On, Submitting your details..."
-            >
-                {/* <p>Some content or children or something.</p> */}
-            </LoadingOverlay>
+        <div className="cont">
+           <div className="loader"></div>
+           <h2>Please Hold On, Submitting your details...</h2>
         </div>
     )
 }
--- src/functions/savetoSheet.js
+++ src/functions/savetoSheet.js
@@ -4,9 +4,9 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
   const SHEET_ID = process.env.REACT_APP_SHEET_ID;
   const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
   const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
-
+    
     exports.handler = async (event, context, callback) => {
-      try {
+      try { 
         const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
         await doc.useServiceAccountAuth({
               client_email: CLIENT_EMAIL,
@@ -14,8 +14,9 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
             });
         await doc.loadInfo();
         const sheet = doc.sheetsByIndex[SHEET_ID];
-        const data =JSON.parse(event.body)
+        const data = JSON.parse(event.body);
         await sheet.addRow(data);
+
         callback(null, {
           // return null to show no errors
           statusCode: 200, // http status code
